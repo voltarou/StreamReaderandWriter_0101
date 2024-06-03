194c66e35a41371c941930919a8ac76551c530d3
voltarou <bagusadnan04@gmail.com>
2024-06-03 11:09:05 +0800

    ofstream outfile

--- infilewithpath/infilewithpath.cpp
+++ infilewithpath/infilewithpath.cpp
@@ -1,10 +1,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
-    
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    ofstream outfile;
+    outfile.open(NamaFile + ",txt", ios::out);
 }
 
 
