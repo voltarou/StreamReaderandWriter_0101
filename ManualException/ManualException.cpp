fb2fc88319c3e42a3dde79e4075d0a11b286de02
voltarou <bagusadnan04@gmail.com>
2024-06-03 11:36:06 +0800

    try catch

---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+
+    catch(int a) {
+        cout << "Pengecualian akan dieksekusi" << endl;
+
+    }
+    catch (...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+}
+
+
