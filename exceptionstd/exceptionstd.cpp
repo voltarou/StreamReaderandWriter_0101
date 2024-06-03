8ce6568e5b2ecead81a87813c861f645b6f97347
voltarou <bagusadnan04@gmail.com>
2024-06-03 11:28:46 +0800

    try catch

---
+++ exceptionstd/exceptionstd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl;
+    try {
+        array<int, 3> data = { 1,2,3 };
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    cout << "Baris Program yang Terakhir" << endl;
+}
+
+
--- infilewithpath/infilewithpath.cpp
+++ infilewithpath/infilewithpath.cpp
@@ -23,6 +23,21 @@ int main()
         outfile << baris << endl;
     }
     outfile.close();
+
+    ifstream infile;
+    infile.open(NamaFile + ",txt", ios::in);
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
 
 
